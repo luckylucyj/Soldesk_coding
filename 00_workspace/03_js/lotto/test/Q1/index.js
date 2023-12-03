190d61734bf773fdbd62ae78019be216adbdc333
luckylucyj <jeyun96@gmail.com>
2023-12-03 20:50:49 +0900

    2023.12.03 pm 08:50 js 문제 풀기

---
+++ 00_workspace/03_js/lotto/test/Q1/index.js
@@ -0,0 +1,25 @@
+var n1=1;
+var n2=2;
+var n3=3;
+var n4=4;
+var n5=5;
+var n6=6;
+
+//로또 게임 만들기
+
+var random1=Math.floor(Math.random()*45+1);
+var random2=Math.floor(Math.random()*45+1);
+var random3=Math.floor(Math.random()*45+1);
+var random4=Math.floor(Math.random()*45+1);
+var random5=Math.floor(Math.random()*45+1);
+var random6=Math.floor(Math.random()*45+1);
+
+var win=0;
+
+if(v1==random1){
+  win=win+1;
+}
+
+while(random2==){
+
+}
